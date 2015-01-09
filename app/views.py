88196331f0b7fbbd1276ca5e2d7310476384eb0e
Patrick Beeson <patrickbeeson@gmail.com>
2015-01-09 14:00:10 -0500

    Add some styling and finish out the views.

--- app/views.py
+++ app/views.py
@@ -22,13 +22,12 @@ ALMANAC_BASE_URL = '{}{}/almanac/q/'.format(
     WUNDERGROUND_API_KEY
 )
 
+
 def geocode_location(location):
     "Get lat and lon coordinates for a zip code"
     try:
         geolocator = Nominatim()
         location = geolocator.geocode(location)
-        # lat = location.latitude
-        # lon = location.longitude
     except Exception as e:
         print('There was a problem geocoding this address: {}'.format(e))
 
@@ -48,30 +47,21 @@ def get_current_temp(lat, lon):
     return int(current_temp)
 
 
-def get_record_high(lat, lon):
-    "Get the record high in F for a given location"
+def get_almanac_data(lat, lon):
+    "Get the almanac data for a given location"
     r = requests.get('{base}{lat},{lon}.json'.format(
         base=ALMANAC_BASE_URL,
         lat=lat,
         lon=lon)
     )
     json_string = r.json()
-    record_high = json_string['almanac']['temp_high']['record']['F']
+    almanac_data = {}
+    almanac_data['record_high'] = json_string['almanac']['temp_high']['record']['F']
+    almanac_data['record_low'] = json_string['almanac']['temp_low']['record']['F']
+    almanac_data['record_high_year'] = json_string['almanac']['temp_high']['recordyear']
+    almanac_data['record_low_year'] = json_string['almanac']['temp_low']['recordyear']
 
-    return int(record_high)
-
-
-def get_record_low(lat, lon):
-    "Get the record low in F for a given location"
-    r = requests.get('{base}{lat},{lon}.json'.format(
-        base=ALMANAC_BASE_URL,
-        lat=lat,
-        lon=lon)
-    )
-    json_string = r.json()
-    record_low = json_string['almanac']['temp_low']['record']['F']
-
-    return int(record_low)
+    return almanac_data
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -87,20 +77,65 @@ def home():
         lon = location.longitude
         print(lat, lon)
         current_temp = get_current_temp(lat, lon)
-        record_high = get_record_high(lat, lon)
-        record_low = get_record_low(lat, lon)
+        almanac_data = get_almanac_data(lat, lon)
+        record_high = int(almanac_data['record_high'])
+        record_low = int(almanac_data['record_low'])
+        record_high_year = int(almanac_data['record_high_year'])
+        record_low_year = int(almanac_data['record_low_year'])
+        temp_diff_high_above = current_temp - record_high
+        temp_diff_high_below = record_high - current_temp
+        temp_diff_low_above = current_temp - record_low
+        temp_diff_low_below = record_low - current_temp
 
         if temp_choice == 'hot':
             if current_temp >= record_high:
-                flash('It has never been this hot!')
+                flash(
+                    """It's never been this hot!
+                    Currently, it's {} degrees, which is {} degrees above the
+                    record of {}, set in {}.""".format(
+                    current_temp,
+                    temp_diff_high_above,
+                    record_high,
+                    record_high_year)
+                    )
             else:
-                flash('It has been this hot before.')
-        elif temp_choice == 'cold':
-            if current_temp >= record_low:
-                flash('It has never been this cold!')
+                flash(
+                    """It's been this hot before.
+                    Currently, it's {} degrees, which is {} degrees below the
+                    record of {}, set in {}.""".format(
+                    current_temp,
+                    temp_diff_high_below,
+                    record_high,
+                    record_high_year)
+                    )
+        else:
+            if current_temp <= record_low:
+                flash(
+                    """It's never been this cold before.
+                    Currently, it's {} degrees, which is {} degrees below the
+                    record of {}, set in {}.""".format(
+                    current_temp,
+                    temp_diff_low_below,
+                    record_low,
+                    record_low_year)
+                    )
             else:
-                flash('It has been this cold before.')
-        return render_template('index.html', form=form)
+                flash(
+                    """It's been this cold before.
+                    Currently, it's {} degrees, which is {} degrees above the
+                    record of {}, set in {}.""".format(
+                    current_temp,
+                    temp_diff_low_above,
+                    record_low,
+                    record_low_year)
+                    )
+        return render_template(
+            'index.html',
+            form=form,
+            current_temp=current_temp,
+            record_high=record_high,
+            record_low=record_low
+        )
     return render_template('index.html', form=form)
 
 
